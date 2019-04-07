e5d70794d0ced7b6e99ab68a696e0817a8845b49
Masaki Kawabata Neto <masaki.kawabata@gmail.com>
2019-04-07 19:08:30 -0300

    adicionado monolog

--- www/index.php
+++ www/index.php
@@ -2,6 +2,26 @@
 require '../config.php';
 require '../vendor/autoload.php';
 
+// load Monolog library
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Formatter\JsonFormatter;
+   
+// create a log channel
+$log = new Logger('channel_name');
+ 
+// create a Json formatter
+$formatter = new JsonFormatter();
+ 
+// create a handler
+$stream = new StreamHandler(__DIR__.'/../application-json.log', Logger::DEBUG);
+$stream->setFormatter($formatter);
+ 
+// bind
+$log->pushHandler($stream);
+ 
+//$log->info('Adding a new user', array('username' => 'Seldaek'));
+
 use \RedBeanPHP\R as R;
 R::setup('sqlite:/tmp/dbfile.db');
 R::freeze( $rb_freeze );
